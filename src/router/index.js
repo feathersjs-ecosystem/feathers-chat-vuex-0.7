b6393f71a114d780b1d98c3e69997ac6d6500ae1
Marshall Thompson <marshall@creativeideal.net>
2017-03-12 07:05:00 -0600

    remove Todos from project

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Todos from '@/components/Todos'
 import Login from '@/components/Login'
 import Signup from '@/components/Signup'
 import Chat from '@/components/Chat/Chat'
@@ -15,7 +14,6 @@ export default new Router({
     { path: '/login', name: 'Login', component: Login },
     { path: '/signup', name: 'Signup', component: Signup },
     { path: '/chat', name: 'Chat', component: Chat },
-    { path: '/todos', name: 'Todos', component: Todos },
     { path: '/services', name: 'Services', component: Services }
   ]
 })
