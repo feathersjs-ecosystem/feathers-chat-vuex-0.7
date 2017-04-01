614dd7a552d834b535e61cd5041717d1db4965d3
Marshall Thompson <marshall@creativeideal.net>
2017-04-01 07:40:19 -0600

    Add comment about login redirect

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,13 @@ import './styles/chat.css'
 export default {
   name: 'app',
   computed: {
+    // The user is automatically set by the feathers-vuex auth module upon login.
     user () {
       return this.$store.state.auth.user
     }
   },
   watch: {
+    // When the user is set, redirect to the Chat page.
     user (newVal) {
       this.$router.replace({name: 'Chat'})
     }
